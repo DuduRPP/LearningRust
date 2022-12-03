3a39a809d3a3d2d6b99e5e2fa99c8537d6d8f1d6
DuduRPP <dudurpp1@gmail.com>
2022-12-03 17:21:34 -0300

    Did guessing game

--- start/hello_cargo/target/debug/deps/hello_cargo-fec4445fe248af7f.d
+++
@@ -1,5 +0,0 @@
-/home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/target/debug/deps/hello_cargo-fec4445fe248af7f.rmeta: src/main.rs
-
-/home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/target/debug/deps/hello_cargo-fec4445fe248af7f.d: src/main.rs
-
-src/main.rs:
--- start/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/target/debug/hello_cargo: /home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/src/main.rs
