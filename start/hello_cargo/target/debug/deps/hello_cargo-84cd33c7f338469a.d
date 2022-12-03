5946e80f09a115dc3fb5bc9a45ada6abf6c64ef6
DuduRPP <dudurpp1@gmail.com>
2022-12-03 15:01:48 -0300

    Did chapter 1 from the book

---
+++ start/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d
@@ -0,0 +1,5 @@
+/home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a: src/main.rs
+
+/home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d: src/main.rs
+
+src/main.rs:
---
+++ start/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/target/debug/hello_cargo: /home/dudurpp/dev/studies/Rust/LearningRust/start/hello_cargo/src/main.rs
